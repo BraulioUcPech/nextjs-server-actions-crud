18fd23de855a734870777381a4192b06fe84e86f
BraulioUcPech <braulioalexson@gmail.com>
2024-05-10 15:56:00 -0600

    feat: Add lazy loading for images in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
+import Taskcard from "@/components/task-card";
 import prisma from "@/lib/prisma";
-import Taskcard from "@/components/task.card";
 
-async function HomepageTitle() {
-  const task = await prisma.task.findMany();
-  console.log(task);
+export const dynamic = "force-dynamic";
+
+async function HomePage() {
+  const tasks = await prisma.task.findMany();
+
   return (
     <div className="grid grid-cols-3 gap-4">
-      {task.map((task) => (
+      {tasks.map((task) => (
         <Taskcard task={task} key={task.id} />
       ))}
     </div>
   );
 }
-export default HomepageTitle;
+
+export default HomePage;
